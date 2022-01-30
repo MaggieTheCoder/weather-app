39bb085e57ddb1e565ef8b00addf401daefe61ff
MaggieTheCoder <mdurandt@gmail.com>
2022-01-30 20:57:03 +0000

    added search bar/input field'

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import ForecastSummaries from "./ForecastSummaries";
 import "../styles/App.css";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
+import SearchForm from "./SearchForm";
 
 const App = () => {
   const [forecasts, setForecasts] = useState([]);
@@ -13,16 +14,24 @@ const App = () => {
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
-  useEffect(() => {
-    getForecast(setSelectedDate, setForecasts, setLocation);
-  }, []);
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
-
+  const [searchText, setSearchText] = useState("");
+  const handleCitySearch = () => {
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+  };
+  useEffect(() => {
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+  }, []);
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
